b569e094d3872997076ebc027270b5568fc614ac
danilorlimma <danilo.rodrigues.lima@hotmail.com>
2025-02-09 23:25:09 -0300

    adiciona fixture e implementa mocka api

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -56,116 +56,125 @@ describe('Hacker Stories', () => {
     })
 
   })
-
-
-  it.skip('shows the footer', () => {
-    cy.get('footer')
-      .should('be.visible')
-      .and('contain', 'Icons made by Freepik from www.flaticon.com')
-  })
-
-  context('List of stories', () => {
-    // Since the API is external,
-    // I can't control what it will provide to the frontend,
-    // and so, how can I assert on the data?
-    // This is why this test is being skipped.
-    // TODO: Find a way to test it out.
-    it.skip('shows the right data for all rendered stories', () => { })
-
-
-    it('shows only nineteen stories after dimissing the first story', () => {
-      cy.get('.button-small')
-        .first()
-        .click()
-
-      cy.get('.item').should('have.length', 19)
-    })
-
-    // Since the API is external,
-    // I can't control what it will provide to the frontend,
-    // and so, how can I test ordering?
-    // This is why these tests are being skipped.
-    // TODO: Find a way to test them out.
-    context.skip('Order by', () => {
-      it('orders by title', () => { })
-
-      it('orders by author', () => { })
-
-      it('orders by comments', () => { })
-
-      it('orders by points', () => { })
-    })
-
-
-  })
-
-  context('Search', () => {
-
-
+  context('Mocking the API', () => {
     beforeEach(() => {
-      cy.intercept('GET', `**/search?query=${newTerm}&page=0`).as('getNewTermStories')
-      cy.get('#search')
-        .clear()
+      cy.intercept('GET',
+       `**/search?query=${newTerm}&page=0`,
+       {fixture: 'stories'} 
+      ).as('getStories')
+      cy.visit('/')
+      cy.wait('@getStories')
     })
 
-    it('types and hits ENTER', () => {
-      cy.get('#search')
-        .type(`${newTerm}{enter}`)
-      cy.wait('@getNewTermStories')
-
-
-      cy.get('.item').should('have.length', 20)
-      cy.get('.item')
-        .first()
-        .should('contain', newTerm)
-      cy.get(`button:contains(${initialTerm})`)
+    it.skip('shows the footer', () => {
+      cy.get('footer')
         .should('be.visible')
+        .and('contain', 'Icons made by Freepik from www.flaticon.com')
     })
 
-    it('types and clicks the submit button', () => {
-      cy.get('#search')
-        .type(newTerm)
-      cy.contains('Submit')
-        .click()
-      cy.wait('@getNewTermStories')
+    context('List of stories', () => {
+      // Since the API is external,
+      // I can't control what it will provide to the frontend,
+      // and so, how can I assert on the data?
+      // This is why this test is being skipped.
+      // TODO: Find a way to test it out.
+      it.skip('shows the right data for all rendered stories', () => { })
 
-      cy.get('.item').should('have.length', 20)
-      cy.get('.item')
-        .first()
-        .should('contain', newTerm)
-      cy.get(`button:contains(${initialTerm})`)
-        .should('be.visible')
+
+      it('shows only nineteen stories after dimissing the first story', () => {
+        cy.get('.button-small')
+          .first()
+          .click()
+
+        cy.get('.item').should('have.length', 19)
+      })
+
+      // Since the API is external,
+      // I can't control what it will provide to the frontend,
+      // and so, how can I test ordering?
+      // This is why these tests are being skipped.
+      // TODO: Find a way to test them out.
+      context.skip('Order by', () => {
+        it('orders by title', () => { })
+
+        it('orders by author', () => { })
+
+        it('orders by comments', () => { })
+
+        it('orders by points', () => { })
+      })
+
+
     })
 
-    context('Last searches', () => {
+    context('Search', () => {
 
 
-      Cypress._.times(2, () => {
-        it('shows a max of 5 buttons for the last searched terms', () => {
-          const faker = require('faker')
-          cy.intercept(
-            'GET',
-            '**/search?query=**'
-          ).as('getRandomStories')
+      beforeEach(() => {
+        cy.intercept('GET', `**/search?query=${newTerm}&page=0`).as('getNewTermStories')
+        cy.get('#search')
+          .clear()
+      })
 
-          Cypress._.times(6, () => {
-            cy.get('#search')
-              .clear()
-              .type(`${faker.random.word()}{enter}`)
-            cy.wait('@getRandomStories')
+      it('types and hits ENTER', () => {
+        cy.get('#search')
+          .type(`${newTerm}{enter}`)
+        cy.wait('@getNewTermStories')
+
+
+        cy.get('.item').should('have.length', 20)
+        cy.get('.item')
+          .first()
+          .should('contain', newTerm)
+        cy.get(`button:contains(${initialTerm})`)
+          .should('be.visible')
+      })
+
+      it('types and clicks the submit button', () => {
+        cy.get('#search')
+          .type(newTerm)
+        cy.contains('Submit')
+          .click()
+        cy.wait('@getNewTermStories')
+
+        cy.get('.item').should('have.length', 20)
+        cy.get('.item')
+          .first()
+          .should('contain', newTerm)
+        cy.get(`button:contains(${initialTerm})`)
+          .should('be.visible')
+      })
+
+      context('Last searches', () => {
+
+
+        Cypress._.times(2, () => {
+          it('shows a max of 5 buttons for the last searched terms', () => {
+            const faker = require('faker')
+            cy.intercept(
+              'GET',
+              '**/search?query=**'
+            ).as('getRandomStories')
+
+            Cypress._.times(6, () => {
+              cy.get('#search')
+                .clear()
+                .type(`${faker.random.word()}{enter}`)
+              cy.wait('@getRandomStories')
+            })
+
+            cy.get('.last-searches button')
+              .should('have.length', 5)
           })
-
-          cy.get('.last-searches button')
-            .should('have.length', 5)
         })
       })
+      // Hrm, how would I simulate such errors?
+      // Since I still don't know, the tests are being skipped.
+      // TODO: Find a way to test them out.
+
+
+
     })
-    // Hrm, how would I simulate such errors?
-    // Since I still don't know, the tests are being skipped.
-    // TODO: Find a way to test them out.
-
-
-
   })
 })
 
